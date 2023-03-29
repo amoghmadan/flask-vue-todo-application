9dd64866a45729998405f79b2217c0d604ca7d54
Amogh Madan <amogh.madan@acsicorp.com>
2023-03-29 14:17:43 +0530

    Make password load_only=True

--- tracker_api/src/schemas/user.py
+++ tracker_api/src/schemas/user.py
@@ -1,6 +1,5 @@
 from marshmallow import fields
 
-
 from models import User
 from utils import ma
 
@@ -8,9 +7,9 @@ from utils import ma
 class UserSchema(ma.SQLAlchemyAutoSchema):
     """User Schema"""
 
-    email = fields.Email()
-    password = fields.String(load_only=True)
+    email = fields.Email(required=True)
 
     class Meta:
         model = User
         load_instance = True
+        load_only = ["password"]
